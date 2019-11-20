5355b16254ac3fbb4a1e255090d218789a7a574f
Bryan <bcromas@gmail.com>
2019-11-20 10:57:43 -0500

    'Start exercise 1'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # git_wkshp
 Practice repo for SODA's workshop on Git.
 
-![SODA logo](https://github.com/Bcromas/git_wkshp/blob/master/assets/SODA_logo.png)
+![SODA logo](https://github.com/Bcromas/git_wkshp/blob/master/assets/soda_small.png)
--- exercise_1.py
+++ exercise_1.py
@@ -1,12 +1,12 @@
-MEMBER_1 = None
-MEMBER_2 = None
-MEMBER_3 = None
+MEMBER_1 = "Vanessa"
+MEMBER_2 = "Steve"
+MEMBER_3 = "Dwight"
 
-MEMBER_1_FOOD = ""
-MEMBER_2_FOOD = ""
-MEMBER_3_FOOD = ""
+MEMBER_1_FOOD = "pumpkin"
+MEMBER_2_FOOD = "stuffing"
+MEMBER_3_FOOD = "carrots"
 
 MEMBERS = [f"MEMBER_{i}" for i in range(1,4)]
 
 for i in MEMBERS:
-    print(i)
+    print(f"{i} doesn't like {MEMBER_1_FOOD}")
