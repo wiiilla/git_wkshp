f655fbeba8900b72756a29fc6f2e17655f5a3c9a
Bryan <bcromas@gmail.com>
2019-11-20 09:22:32 -0500

    'Initial set up'

---
+++ exercise_1.py
@@ -0,0 +1,12 @@
+MEMBER_1 = None
+MEMBER_2 = None
+MEMBER_3 = None
+
+MEMBER_1_FOOD = ""
+MEMBER_2_FOOD = ""
+MEMBER_3_FOOD = ""
+
+MEMBERS = [f"MEMBER_{i}" for i in range(1,4)]
+
+for i in MEMBERS:
+    print(i)
