93932b5e2514a7fd5eff34f2fc02ea116c5ceb58
Bryan <bcromas@gmail.com>
2019-11-21 22:16:43 -0500

    change to hometowns

--- exercise_1.py
+++ exercise_1.py
@@ -2,15 +2,15 @@ MEMBER_1 = "Abigail"
 MEMBER_2 = "Allen"
 MEMBER_3 = "Ramon"
 
-MEMBER_1_FOOD = "cheese"
-MEMBER_2_FOOD = "pickles"
-MEMBER_3_FOOD = "corn"
+MEMBER_1_HOME = "Richmond, VA"
+MEMBER_2_HOME = "Haiphong, Viet Nam"
+MEMBER_3_HOME = "Monterrey, Mexico"
 
 MEMBERS = {
-    MEMBER_1:MEMBER_1_FOOD,
-    MEMBER_2:MEMBER_2_FOOD,
-    MEMBER_3:MEMBER_3_FOOD,
+    MEMBER_1:MEMBER_1_HOME,
+    MEMBER_2:MEMBER_2_HOME,
+    MEMBER_3:MEMBER_3_HOME,
 }
 
 for k,v in MEMBERS.items():
-    print(f"{k} doesn't like {v}")
+    print(f"{k} is from {v}")
