b138de5ef721aad00ca59712efcd03da64ab9879
Elham Amini <eamini@umich.edu>
2019-11-22 17:40:56 -0500

    Elham's change

--- exercise_1.py
+++ exercise_1.py
@@ -1,9 +1,9 @@
 MEMBER_1 = "Willa"
-MEMBER_2 = "Allen"
+MEMBER_2 = "Elham"
 MEMBER_3 = "Ramon"
 
 MEMBER_1_HOME = "Shanghai, China"
-MEMBER_2_HOME = "Haiphong, Viet Nam"
+MEMBER_2_HOME = "Tehran, Iran"
 MEMBER_3_HOME = "Monterrey, Mexico"
 
 MEMBERS = {
